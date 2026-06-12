563318f8fccc2f438ed158c0989a94e2125d87f3
Jose Simancas <josesimancas51@gmail.com>
2026-06-12 18:29:59 -0500

    add some routes

---
+++ app/levels/page.tsx
@@ -0,0 +1,7 @@
+export default function DigimonPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Levels</h1>
+    </main>
+  )
+}
---
+++ app/types/page.tsx
@@ -0,0 +1,7 @@
+export default function DigimonPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold">Listado de types</h1>
+    </main>
+  )
+}
