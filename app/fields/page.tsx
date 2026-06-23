277baeaa7d498ed02313025d16f0da674f7421cb
Jose Simancas <josesimancas51@gmail.com>
2026-06-23 11:53:08 -0500

    update header

--- app/fields/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DigimonPage() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">Listado de campos</h1>
-    </main>
-  )
-}
--- app/levels/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DigimonPage() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">Levels</h1>
-    </main>
-  )
-}
